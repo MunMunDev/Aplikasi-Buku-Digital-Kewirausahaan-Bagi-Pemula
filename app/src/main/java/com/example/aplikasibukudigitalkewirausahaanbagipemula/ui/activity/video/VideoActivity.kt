8c75b96a0f0b43145da3ad149991cc4f9e5963b5
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-08 17:34:52 +0800

    tambah data bagian di read pdf activity

---
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/ui/activity/video/VideoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasibukudigitalkewirausahaanbagipemula.ui.activity.video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
