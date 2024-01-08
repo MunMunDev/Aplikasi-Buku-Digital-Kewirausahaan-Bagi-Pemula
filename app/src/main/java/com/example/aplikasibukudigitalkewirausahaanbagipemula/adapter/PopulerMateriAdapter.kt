93552401995801c512b4a33eb216ed2f227f6eab
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-08 17:24:28 +0800

    add mainActivity

---
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/adapter/PopulerMateriAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.aplikasibukudigitalkewirausahaanbagipemula.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.data.model.MateriModel
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.databinding.ListDataPopulerBinding
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.ui.activity.read_pdf.ReadPdfActivity
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.utils.Constant
+
+class PopulerMateriAdapter(private var listMateri: ArrayList<MateriModel>): RecyclerView.Adapter<PopulerMateriAdapter.ViewHolder>() {
+    class ViewHolder(val binding: ListDataPopulerBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListDataPopulerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return if(listMateri.size>2){
+            3
+        } else{
+            listMateri.size
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dataMateri = listMateri[position]
+        holder.apply {
+            binding.apply {
+                Glide.with(itemView.context)
+                    .load("${Constant.BASE_URL}${dataMateri.lokasiFile}${dataMateri.urlImage}") // URL Gambar
+                    .error(R.drawable.image_book)
+                    .into(ivGambarMateri) // imageView mana yang akan diterapkan
+
+                tvJudulMateri.text = dataMateri.namaMateri
+                var namaPenulis = ""
+                namaPenulis = try {
+                    val arrayNamaPenulis = dataMateri.namaPenulis!!.split(";-")
+                    arrayNamaPenulis[0]
+                } catch (ex: Exception){
+                    dataMateri.namaPenulis!!
+                }
+                tvNamaPemateri.text = namaPenulis
+            }
+            itemView.apply {
+                this.setOnClickListener{
+                    Toast.makeText(context, "click", Toast.LENGTH_SHORT).show()
+                    context.startActivity(Intent(context, ReadPdfActivity::class.java))
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/KontrolNavigationDrawer.kt
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/KontrolNavigationDrawer.kt
@@ -2,15 +2,15 @@ package com.example.aplikasibukudigitalkewirausahaanbagipemula.utils
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.view.View
-import android.widget.Button
+import android.content.res.Resources
 import android.widget.ImageView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.bumptech.glide.load.engine.Resource
 import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.ui.activity.main.MainActivity
 
 class KontrolNavigationDrawer(var context: Context) {
     var sharedPreferences = SharedPreferencesLogin(context)
@@ -27,58 +27,65 @@ class KontrolNavigationDrawer(var context: Context) {
     @SuppressLint("ResourceAsColor")
     fun onClickItemNavigationDrawer(navigation: com.google.android.material.navigation.NavigationView, navigationLayout: DrawerLayout, igNavigation:ImageView, activity: Activity){
         navigation.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.userNavDrawerHome -> {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                    activity.finish()
+                }
+            }
             if(sharedPreferences.getSebagai() == "user"){
                 when(it.itemId){
-                    R.id.userNavDrawerHome ->{
-                        val intent = Intent(context, MainActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.userNavDrawerRiwayatPembayaran ->{
-                        val intent = Intent(context, RiwayatPembayaranActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.userNavDrawerAkun ->{
-                        val intent = Intent(context, AkunActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.userBtnKeluar ->{
-                        logout(activity)
-                    }
+//                    R.id.userNavDrawerHome ->{
+//                        val intent = Intent(context, MainActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.userNavDrawerRiwayatPembayaran ->{
+//                        val intent = Intent(context, RiwayatPembayaranActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.userNavDrawerAkun ->{
+//                        val intent = Intent(context, AkunActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.userBtnKeluar ->{
+//                        logout(activity)
+//                    }
                 }
             }
             else if(sharedPreferences.getSebagai() == "admin"){
                 when(it.itemId){
-                    R.id.adminNavDrawerHome ->{
-                        val intent = Intent(context, AdminMainActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.adminNavDrawerPerumahan->{
-                        val intent = Intent(context, AdminPerumahanActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.adminNavDrawerBiaya->{
-                        val intent = Intent(context, AdminBiayaActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.adminNavDrawerPembayaran->{
-                        val intent = Intent(context, AdminPembayaranActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.adminNavDrawerAkun ->{
-                        val intent = Intent(context, AdminUsersActivity::class.java)
-                        context.startActivity(intent)
-                        activity.finish()
-                    }
-                    R.id.btnAdminKeluar ->{
-                        logout(activity)
-                    }
+//                    R.id.adminNavDrawerHome ->{
+//                        val intent = Intent(context, AdminMainActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.adminNavDrawerPerumahan->{
+//                        val intent = Intent(context, AdminPerumahanActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.adminNavDrawerBiaya->{
+//                        val intent = Intent(context, AdminBiayaActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.adminNavDrawerPembayaran->{
+//                        val intent = Intent(context, AdminPembayaranActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.adminNavDrawerAkun ->{
+//                        val intent = Intent(context, AdminUsersActivity::class.java)
+//                        context.startActivity(intent)
+//                        activity.finish()
+//                    }
+//                    R.id.btnAdminKeluar ->{
+//                        logout(activity)
+//                    }
                 }
 
             }
@@ -93,23 +100,23 @@ class KontrolNavigationDrawer(var context: Context) {
     }
 
     fun logout(activity: Activity){
-        val viewAlertDialog = View.inflate(context, R.layout.alert_dialog_logout, null)
-        val btnLogout = viewAlertDialog.findViewById<Button>(R.id.btnLogout)
-        val btnBatalLogout = viewAlertDialog.findViewById<Button>(R.id.btnBatalLogout)
-
-        val alertDialog = AlertDialog.Builder(context)
-        alertDialog.setView(viewAlertDialog)
-        val dialog = alertDialog.create()
-        dialog.show()
-
-        btnLogout.setOnClickListener {
-            sharedPreferences.setLogin(0, "", "","", "","", "","")
-//            sharedPreferences.setLogin(0, "","", "","", "","", "")
-            context.startActivity(Intent(context, LoginActivity::class.java))
-            activity.finish()
-        }
-        btnBatalLogout.setOnClickListener {
-            dialog.dismiss()
-        }
+//        val viewAlertDialog = View.inflate(context, R.layout.alert_dialog_logout, null)
+//        val btnLogout = viewAlertDialog.findViewById<Button>(R.id.btnLogout)
+//        val btnBatalLogout = viewAlertDialog.findViewById<Button>(R.id.btnBatalLogout)
+//
+//        val alertDialog = AlertDialog.Builder(context)
+//        alertDialog.setView(viewAlertDialog)
+//        val dialog = alertDialog.create()
+//        dialog.show()
+//
+//        btnLogout.setOnClickListener {
+//            sharedPreferences.setLogin(0, "", "","", "","", "","")
+////            sharedPreferences.setLogin(0, "","", "","", "","", "")
+//            context.startActivity(Intent(context, LoginActivity::class.java))
+//            activity.finish()
+//        }
+//        btnBatalLogout.setOnClickListener {
+//            dialog.dismiss()
+//        }
     }
 }
