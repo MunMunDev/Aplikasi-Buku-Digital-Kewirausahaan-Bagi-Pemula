1f3fe9b2e999d9b5d3eebe85d2ceee872e1e4067
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-12 14:32:06 +0800

    mengubah base url

--- app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
@@ -1,6 +1,6 @@
 package com.example.aplikasibukudigitalkewirausahaanbagipemula.utils
 
 object Constant {
-//    const val BASE_URL = "http://192.168.1.7/"
-    const val BASE_URL = "http://192.168.167.91/"
+    const val BASE_URL = "http://192.168.1.2/"
+//    const val BASE_URL = "http://192.168.167.91/"
 }
