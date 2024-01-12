9c15acee4cb76d414ed908486bf1e68f84d3a13b
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-12 19:53:54 +0800

    mengatur layout dan mengatur data admin main

--- app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/ui/activity/admin/main/AdminMainActivity.kt
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/ui/activity/admin/main/AdminMainActivity.kt
@@ -3,10 +3,40 @@ package com.example.aplikasibukudigitalkewirausahaanbagipemula.ui.activity.admin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.databinding.ActivityAdminMainBinding
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.utils.KontrolNavigationDrawer
 
 class AdminMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminMainBinding
+    private lateinit var kontrolNavigationDrawer: KontrolNavigationDrawer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_main)
+        binding = ActivityAdminMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setKontrolNavigationDrawer()
+        setButton()
+    }
+
+    private fun setKontrolNavigationDrawer() {
+        binding.apply {
+            kontrolNavigationDrawer = KontrolNavigationDrawer(this@AdminMainActivity)
+            kontrolNavigationDrawer.cekSebagai(navView)
+            kontrolNavigationDrawer.onClickItemNavigationDrawer(navView, drawerLayoutMain, ivDrawerView, this@AdminMainActivity)
+        }
+    }
+
+    private fun setButton() {
+        binding.apply {
+            cvMateri.setOnClickListener {
+
+            }
+            cvVideo.setOnClickListener {
+
+            }
+            cvAkun.setOnClickListener {
+
+            }
+        }
     }
 }
