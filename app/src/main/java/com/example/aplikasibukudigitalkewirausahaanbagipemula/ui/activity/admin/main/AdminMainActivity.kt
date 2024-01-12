7358c1888379bbbfb4aa73cf50145c6157b8ab8d
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-12 19:20:08 +0800

    membuat admin main activity

---
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/ui/activity/admin/main/AdminMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasibukudigitalkewirausahaanbagipemula.ui.activity.admin.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+
+class AdminMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_main)
+    }
+}
