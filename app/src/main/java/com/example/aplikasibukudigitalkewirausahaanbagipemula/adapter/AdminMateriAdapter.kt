812501b792f5070963cfe5ee339be5f8750ee448
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-13 16:12:57 +0800

    tambah adapter materi untuk admin

---
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/adapter/AdminMateriAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.aplikasibukudigitalkewirausahaanbagipemula.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.R
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.data.model.MateriModel
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.databinding.ListDataSemuaBinding
+import com.example.aplikasibukudigitalkewirausahaanbagipemula.utils.Constant
+
+class AdminMateriAdapter(
+    private var listMateri: ArrayList<MateriModel>,
+    private var click: ClickButton
+) : RecyclerView.Adapter<AdminMateriAdapter.ViewHolder>() {
+
+    var tempMateri = listMateri
+    fun searchData(kata: String){
+        val vKata = kata.toLowerCase().trim()
+        var data = listMateri.filter {
+            it.namaMateri!!.lowercase().trim().contains(vKata)
+        } as ArrayList
+        tempMateri = data
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val binding: ListDataSemuaBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListDataSemuaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return tempMateri.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dataMateri = tempMateri[position]
+        holder.apply {
+            binding.apply {
+                Glide.with(itemView.context)
+                    .load("${Constant.BASE_URL}${dataMateri.lokasiFile}${dataMateri.urlImage}") // URL Gambar
+                    .error(R.drawable.image_book)
+                    .into(ivGambarMateri) // imageView mana yang akan diterapkan
+
+                var namaPenulis = ""
+                namaPenulis = try {
+                    val arrayNamaPenulis = dataMateri.namaPenulis!!.split(";-")
+                    arrayNamaPenulis[0]
+                } catch (ex: Exception){
+                    dataMateri.namaPenulis!!
+                }
+                tvNamaPemateri.text = namaPenulis
+                tvJudulMateri.text = dataMateri.namaMateri
+                tvJumlahDilihat.text = "${dataMateri.jumlahPelihat}x Di Baca"
+                btnBuka.text = "Setting"
+                btnBuka.setOnClickListener {
+                    click.clickItem(dataMateri, it)
+                }
+            }
+            itemView.apply {
+                this.setOnClickListener{
+
+                }
+            }
+        }
+    }
+
+    interface ClickButton{
+        fun clickItem(data: MateriModel, it:View)
+    }
+}
