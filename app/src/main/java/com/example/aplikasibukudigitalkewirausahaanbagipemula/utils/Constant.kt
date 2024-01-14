fd7913968f41dd138f1181c8352f197add4863fe
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-15 01:43:52 +0800

    menambahkan materi dan video url

--- app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
@@ -1,6 +1,9 @@
 package com.example.aplikasibukudigitalkewirausahaanbagipemula.utils
 
 object Constant {
-//    const val BASE_URL = "http://192.168.1.2/"
-    const val BASE_URL = "http://192.168.167.91/"
+    const val BASE_URL = "http://192.168.1.3/"
+//    const val BASE_URL = "http://192.168.167.91/"
+
+    const val MATERI_URL = "buku-digital-kewirausahaan/materi/"
+    const val VIDEO_URL = "buku-digital-kewirausahaan/video/"
 }
