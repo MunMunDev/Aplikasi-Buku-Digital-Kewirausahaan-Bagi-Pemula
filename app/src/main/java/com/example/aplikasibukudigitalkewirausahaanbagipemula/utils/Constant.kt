7fb002fc6268595bf3069443c3782b98c7cabc1b
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-01-24 00:18:14 +0800

    menambahkan code untuk mengecek apakah file yang diambil adalah pdf atau image

--- app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
+++ app/src/main/java/com/example/aplikasibukudigitalkewirausahaanbagipemula/utils/Constant.kt
@@ -1,11 +1,12 @@
 package com.example.aplikasibukudigitalkewirausahaanbagipemula.utils
 
 object Constant {
-    const val BASE_URL = "http://192.168.1.3/"
+    const val BASE_URL = "http://192.168.1.6/"
 //    const val BASE_URL = "http://192.168.167.91/"
 
     const val MATERI_URL = "buku-digital-kewirausahaan/materi/"
-    const val VIDEO_URL = "buku-digital-kewirausahaan/video/"
 
     const val STORAGE_PERMISSION_CODE = 10
+    const val PDF_CODE = 11
+    const val IMAGE_CODE = 12
 }
